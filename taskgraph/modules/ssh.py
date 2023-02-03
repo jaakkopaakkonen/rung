d05b7a86638fbd0d113b46938eee228aacce3547
Jaakko Paakkonen <jaakko.t.paakkonen@gmail.com>
2023-02-03 15:17:02 +0200

    task remove_ssh_key

--- taskgraph/modules/ssh.py
+++ taskgraph/modules/ssh.py
@@ -1,8 +1,14 @@
 from taskgraph.task import task_func, task_shell_script
 
 task_shell_script(
-    "ssh-keygen -f ~/.ssh/known_hosts -R {host}\n" +
-    "ssh-keyscan {host} >> ~/.ssh/known_hosts\n",
+    "ssh-keyscan {host} >> ~/.ssh/known_hosts",
     "update_ssh_key",
+    "remove_ssh_key",
+    "host",
+)
+
+task_shell_script(
+    "ssh-keygen -f ~/.ssh/known_hosts -R {host}",
+    "remove_ssh_key",
     "host",
 )
